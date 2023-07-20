28dbba0872027640e6858726fde8516f7db4a2e0
Aimee.Fullerton <1365855290113302@mil>
2023-07-20 16:26:26 -0700

    capitalize comid

--- code/0_functions/fncExtractLookup.R
+++ code/0_functions/fncExtractLookup.R
@@ -2,7 +2,7 @@
 # adds new columns to data for 'comid', 'tim.year', 'tim.doy', and 'tim.date'
 fncExtractLookup <- function(the_data){
   out <- strsplit(the_data$lookup, "_")
-  the_data$comid <- as.numeric(do.call(rbind, out)[,1])
+  the_data$COMID <- as.numeric(do.call(rbind, out)[,1])
   the_data$tim.year <- as.numeric(do.call(rbind, out)[,2])
   the_data$tim.doy <- as.numeric(do.call(rbind, out)[,3])
   the_data$tim.date <- as.Date(the_data$tim.doy, origin = as.Date(paste0(the_data$tim.year, "-01-01")))
