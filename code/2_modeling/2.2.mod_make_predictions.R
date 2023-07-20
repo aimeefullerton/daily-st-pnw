a28572897588cce63815c0c58ba00a019e24bd2b
Aimee.Fullerton <1365855290113302@mil>
2023-07-20 12:37:46 -0700

    reinstate huc10list; load models from correct directory

--- code/2_modeling/2.2.mod_make_predictions.R
+++ code/2_modeling/2.2.mod_make_predictions.R
@@ -25,8 +25,7 @@ COM_HUC <- data.table::fread("data/COMID_to_HUC12.csv")
 COM_HUC$Huc6 <- substr(COM_HUC$Huc12, 1, 6)
 COM_HUC$Huc10 <- substr(COM_HUC$Huc12, 1, 10)
 huclist <- sort(unique(COM_HUC$Huc6))
-#h <- sort(unique(COM_HUC$Huc10)) #all possible
-h <- dir(huc_path); h <- gsub("huc_", "", h); h <- gsub(".fst", "", h) #from directory (had sufficient data)
+huc10list <- dir(huc_path); h <- gsub("huc_", "", h); h <- gsub(".fst", "", h)
 spatial_data <- data.table::fread("data/spatial_data.csv")
 
 # To run a subset:
@@ -34,8 +33,8 @@ spatial_data <- data.table::fread("data/spatial_data.csv")
 # huc10list <- huc10list[grep("170102", h)]
 
 # LOAD FITTED MODELS ----
-load(paste0(models_path, "/fitted_model_full.RData"))
-load(paste0(models_path, "/antec_air_temp_duration_models_full.RData"))
+load(paste0(models_path, "/fitted_model.RData"))
+load(paste0(models_path, "/antec_air_temp_duration_models.RData"))
 
 #Make model more efficient
 strip_glm = function(cm) {
