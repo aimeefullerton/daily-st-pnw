817e4e25f921dc9abf1d0fba1e870ccba31bad49
Aimee.Fullerton <1365855290113302@mil>
2023-07-21 14:40:45 -0700

    default choice = "full"

--- code/0_functions/fncChoose.R
+++ code/0_functions/fncChoose.R
@@ -1,6 +1,6 @@
 fncChoose <- function(choices){
   print(choices)
   idx <- readline("Choose which dataset to use by its number ")
-  return(choices[as.numeric(idx)])
+  ifelse(idx == "", return(choices[1]), return(choices[as.numeric(idx)]))
 }
 #usage: fncChoose(choices = c("full", "season", "region", "season-region"))
